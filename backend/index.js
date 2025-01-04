5e08c99ad31bc52dabe366069752dc5a3d195243
gregory <gregorykimbira7@gmail.com>
2025-01-04 18:45:51 +0300

    created a login /sign up server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose= require('mongoose');
+const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const port = 3000
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//connection
+mongoose.connect('mongodb://localhost:27017/login-signup', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
 

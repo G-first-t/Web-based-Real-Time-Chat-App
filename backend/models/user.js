6901fe4e47bcbbbc1acb7c038524d51a1710ff5b
gregory <gregorykimbira7@gmail.com>
2025-01-04 18:04:11 +0300

    created the full user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,21 @@
 
 const mongoose=require('mongoose');
-
+const bcrypt=require('bcrypt');
 const userSchema= new mongoose.Schema({
     username:{type:'string'},
     email:{type:'string',required:true, unique:true},
     password:{type:'string',required:true}
 });
-//saving p
+//saving password before sending them to db
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash( this.password,10);
+    next();
+})
+
+//comparing password
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+ module.exports=mongoose.model('User', userSchema)

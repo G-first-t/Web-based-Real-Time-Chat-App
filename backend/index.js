ade40ab33c3b63017ceaddaf948f6e8ba717b9fe
gregory <gregorykimbira7@gmail.com>
2025-01-05 11:27:37 +0300

    connecting with socket.Io

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,45 @@ const mongoose= require('mongoose');
 const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
+const {socketIo} = require('socket.io');
+const http= require('http');
 const port = 3000
+
+
+const server = http.createServer(app);
+const io= new socketIo(server);
+
+
+let users = {};
+io.on('connection', (socket)=>{
+    console.log('User connected:', socket.id);
+
+   //registering users
+    socket.on('register', (userId) => {
+        users[userId] = socket.id;
+        console.log(`User ${userId} connected with socket ID ${socket.id}`);
+    });
+
+    //sending private message
+    socket.on('privateMessage', ({ recipientId, message }) => {
+        const recipientSocketId = users[recipientId];
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('privateMessage', message);
+        }
+    });
+
+    // disconnecting user
+    socket.on('disconnect', () => {
+        for (let userId in users) {
+            if (users[userId] === socket.id) {
+                delete users[userId];
+                break;
+            }
+        }
+        console.log('User disconnected:', socket.id);
+    });
+
+});
 
 //middleware
 app.use(cors());

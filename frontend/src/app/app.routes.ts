5fabce407b54f6fbdf3c61f32ae02a170fccee4e
gregory <gregorykimbira7@gmail.com>
2025-01-05 11:52:00 +0300

    adding routes to the file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './pages/Login/login.component';
+import {SignupComponent} from './pages/SignUp/signup.component';
+import {ChatComponent} from './pages/chatting/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'chat', component: ChatComponent }
+];

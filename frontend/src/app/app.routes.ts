12121baeb8ef7d16a72270726560669af520a9c0
gregory <gregorykimbira7@gmail.com>
2025-01-05 16:38:57 +0300

    delete landing page routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import {LoginComponent} from './pages/Login/login.component';
 import {SignupComponent} from './pages/SignUp/signup.component';
 import {ChatComponent} from './pages/chatting/chat.component';
 
+
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+
+  { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'chat', component: ChatComponent }
 ];

23687421d3db2545cfc68308e2cfec0b8b05e17c
gregory <gregorykimbira7@gmail.com>
2025-01-06 11:36:10 +0300

    deleting routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,4 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from './pages/Login/login.component';
-import {SignupComponent} from './pages/SignUp/signup.component';
-import {ChatComponent} from './pages/chatting/chat.component';
-
 
 export const routes: Routes = [
-
-  { path: '', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'chat', component: ChatComponent }
-];
+]
